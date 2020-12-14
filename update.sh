daccc490c20e140037c5f710f25e148113e242f7
Michael Shihjay Chen <shihjay2@gmail.com>
2020-12-13 23:19:38 -0800

    Add update script

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# update script
+
+set -e
+
+# Iterate AS containers
+SUFFIX="$(basename $PWD)" &&
+docker-compose pull
+docker-compose down
+docker volume rm ${SUFFIX}_nosh_public
+docker-compose up -d
+
+# Clean docker images
+docker system prune -f
+exit 0
